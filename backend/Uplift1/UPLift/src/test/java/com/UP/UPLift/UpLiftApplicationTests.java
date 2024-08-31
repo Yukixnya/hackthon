87705d80de55d1da69de25905a7f919ae74611b0
ユキ <zero006607578@gmail.com>
2024-08-31 16:16:47 +0530

    Add files via upload

---
+++ backend/Uplift1/UPLift/src/test/java/com/UP/UPLift/UpLiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UP.UPLift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpLiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
